6334747727f1c2bc84f058e5a11b4aef28a2531c
Mohan Sharma <mohan.sharma@nextuple.com>
2021-01-31 13:18:50 +0530

    Updated script to print cwd and using httpie ignore stdin

--- dns-sync.sh
+++ dns-sync.sh
@@ -4,12 +4,13 @@ set -e
 echo "Running at $(date)"
 cd "${1}"
 . dns.config
+echo "Running from $(pwd)"
 printf "Generating access token..."
-AUTH=$(http POST https://login.microsoftonline.com/${TENANT_ID}/oauth2/token content-type:"application/x-www-form-urlencoded" grant_type=client_credentials client_id="${CLIENT_ID}" client_secret="${CLIENT_SECRET}" resource="https://management.azure.com" --form)
+AUTH=$(http POST https://login.microsoftonline.com/${TENANT_ID}/oauth2/token content-type:"application/x-www-form-urlencoded" grant_type=client_credentials client_id="${CLIENT_ID}" client_secret="${CLIENT_SECRET}" resource="https://management.azure.com" --form --ignore-stdin)
 ACCESS_TOKEN=$(echo "${AUTH}" | jq -r '.access_token')
 printf " received of length : %s\n" "${#ACCESS_TOKEN}"
 printf "Requesting DNS records..."
-DNS_RESPONSE=$(http GET https://management.azure.com/subscriptions/${SUBSCRIPTION_ID}/resourceGroups/${RESOURCE_GROUP}/providers/Microsoft.Network/privateDnsZones/${ZONE_NAME}/ALL api-version==2018-09-01 Authorization:"Bearer ${ACCESS_TOKEN}")
+DNS_RESPONSE=$(http GET https://management.azure.com/subscriptions/${SUBSCRIPTION_ID}/resourceGroups/${RESOURCE_GROUP}/providers/Microsoft.Network/privateDnsZones/${ZONE_NAME}/ALL api-version==2018-09-01 Authorization:"Bearer ${ACCESS_TOKEN}" --ignore-stdin)
 printf " received of length : %s\n" "${#DNS_RESPONSE}"
 RECORDS=$(echo "${DNS_RESPONSE}" | jq -c '.value[]')
 for RECORD in ${RECORDS}; do
